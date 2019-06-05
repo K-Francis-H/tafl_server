4a5ef8878242a5bc9e9dbb47dac2cc87db27668c
Kyle Harrity <hpkyh47@gmail.com>
2019-06-05 00:09:21 -0400

    cleveland rules work

--- site/ClevelandRules.js
+++ site/ClevelandRules.js
@@ -15,18 +15,6 @@ function ClevelandRules(){
 	const WHITE_MASK = W | K;
 	const PIECE_MASK = BLACK_MASK | WHITE_MASK;
 
-	//var states = [];
-	//states.push(variant);
-
-	var state = variant;
-	var moves = [];
-	var size = variant.length;
-
-	var moveCount = 0;
-	var currentPlayer = player || B; //black traditionally goes first, but this may be a simulated move
-
-	//var selectedPiece = null;
-
 	this.getMoves = function(player){
 		console.log(player);
 		if(player != W && player != B){
@@ -38,44 +26,12 @@ function ClevelandRules(){
 		for(var i=0; i < state.length; i++){
 			for(var j=0; j < state[i].length; j++){
 				if( (state[i][j] & mask) > 0){
-					newMoves = newMoves.concat(getMovesForPieceAtPosition(i,j));
+					newMoves = newMoves.concat(getMovesForPieceAtPosition(state, i,j));
 				}
 			}
 		}
 		return newMoves;
 	};
-
-	//this.getMovesForSelectedPiece = function(i,j){
-	//	return getMovesForPieceAtPosition(i,j);
-	//};
-
-	/*this.setSelectedPiece(selPiece){
-		selectedPiece = selPiece;
-	};
-
-	this.getSelectedPiece(selPiece){
-		return selectedPiece;
-	};*/
-
-	this.getAnnotatedBoard = function(selectedPiece){
-		let annoState = JSON.parse(JSON.stringify(state));
-		let lm = lastMove();
-		if(lm){//apply last state annotations
-			annoState[lm.sx][lm.sy] |= LAST_MOVE;
-			annoState[lm.ex][lm.ey] |= LAST_MOVE;
-		}
-		if(selectedPiece){//apply selected piece and show available moves
-			annoState[selectedPiece.x][selectedPiece.y] |= SELECTED;
-			let availableMoves = getMovesForPieceAtPosition(selectedPiece.x,selectedPiece.y);
-			//console.log(availableMoves);
-			for(let i=0; i < availableMoves.length; i++){
-				let move = availableMoves[i];
-				annoState[move.ex][move.ey] |= VALID_MOVE;
-			} 
-		}
-		//console.log(annoState);
-		return annoState;
-	}
 
 	function Move(sx, sy, ex, ey, player){
 		//this.start = {x:sx,y:sy};
@@ -87,9 +43,9 @@ function ClevelandRules(){
 		this.player = player; //TODO may be inferrable from board state
 	};
 
-	this.getMovesForPieceAtPosition = function(i, j, player){ //think its inferrable
+	this.getMovesForPieceAtPosition = function(state, i, j, player){ //think its inferrable
 
-		let isK = isKing(i,j);
+		let isK = isKing(state, i,j);
 		let limitFunc = function(x,y){
 			//king may only leave the kings hall, never enter, all others cannot ever be in any special square
 			return isK || !isSpecialCell(x,y);//(isK && !isKingsHall(x,y)) || !isSpecialCell(x,y); 
@@ -103,7 +59,7 @@ function ClevelandRules(){
 			if((state[k][j] & PIECE_MASK) === 0 && limitFunc(k,j)){
 				pieceMoves.push(new Move(i,j,k,j,player));
 			}
-			else if((state[k][j] & PIECE_MASK) === 0 && isKingsHall(k,j)){
+			else if((state[k][j] & PIECE_MASK) === 0 && isKingsHall(state, k,j)){
 				continue; //may pass over if it was empty
 			}
 			else{
@@ -115,7 +71,7 @@ function ClevelandRules(){
 			if((state[k][j] & PIECE_MASK) === 0 && limitFunc(k,j)){
 				pieceMoves.push(new Move(i,j,k,j,player));
 			}
-			else if((state[k][j] & PIECE_MASK) === 0 && isKingsHall(k,j)){
+			else if((state[k][j] & PIECE_MASK) === 0 && isKingsHall(state, k,j)){
 				continue; //may pass over if it was empty
 			}
 			else{
@@ -127,7 +83,7 @@ function ClevelandRules(){
 			if((state[i][k] & PIECE_MASK) === 0 && limitFunc(i,k)){
 				pieceMoves.push(new Move(i,j,i,k,player));
 			}
-			else if((state[i][k] & PIECE_MASK) === 0 && isKingsHall(i,k)){
+			else if((state[i][k] & PIECE_MASK) === 0 && isKingsHall(state, i,k)){
 				continue; //may pass over if it was empty
 			}
 			else{
@@ -139,7 +95,7 @@ function ClevelandRules(){
 			if((state[i][k] & PIECE_MASK) === 0 && limitFunc(i,k)){
 				pieceMoves.push(new Move(i,j,i,k,player));
 			}
-			else if((state[i][k] & PIECE_MASK) === 0 && isKingsHall(i,k)){
+			else if((state[i][k] & PIECE_MASK) === 0 && isKingsHall(state, i,k)){
 				continue; //may pass over if it was empty
 			}
 			else{
@@ -155,29 +111,32 @@ function ClevelandRules(){
 		return moves[moves.length-1];
 	}
 
-	function isCorner(i, j){
+	function isCorner(state, i, j){
+		let size = state.length;
 		return i === 0 && j === 0 
 		|| i === size-1 && j === 0
 		|| i === size-1 && j === size-1
 		|| i === 0 && j === size-1;
 	}
 
-	function isKingsHall(i, j){
+	function isKingsHall(state, i, j){
+		let size = state.length;
 		return i === Math.floor(size/2) && j === Math.floor(size/2);
 	}
 
-	function isSpecialCell(x, y){
-		return isKingsHall(x, y) || isCorner(x, y);
+	function isSpecialCell(state, x, y){
+		return isKingsHall(state, x, y) || isCorner(state, x, y);
 	}
 
-	function isKing(i,j){
+	function isKing(state, i,j){
 		return (state[i][j] & K) > 0;
 	}
 
 	//should be called AFTER performing the move on the game state
 	//TODO need to check captures against center to see if it is occupied by king. if so white pieces are uncapturable against it
 	//TODO use notation and this to auto score games for the AI
-	function checkCaptures(move){
+	this.checkCaptures = function(state, move){
+		let size = state.length;
 		//TODO use in the (done)makeMove, simulateMove, and (done)undo functions (move needs to store capture metadata in an array)
 		let color = (state[move.ex][move.ey] & PIECE_MASK) === B ? B : WHITE_MASK;
 		//to make sure its not an empty square somehow
@@ -195,22 +154,22 @@ function ClevelandRules(){
 		let position = {x : move.ex, y : move.ey};
 		//TODO seems to also capture empty spaces, not a huge deal since undo wont affect the board state in this scenario but still
 		//if capture help position exists AND (adjacent position is empty AND capture help position is empty AND KINGS_HALL OR CORNER OR allied piece
-		if(position.x+2 < size && (state[position.x+1][position.y] & color) === 0 && ( ( (state[position.x+2][position.y] & PIECE_MASK) === 0 && isKingsHall(position.x+2,position.y) || isCorner(position.x+2,position.y) ) || (state[position.x+2][position.y] & color) > 0) ){
+		if(position.x+2 < size && (state[position.x+1][position.y] & color) === 0 && ( ( (state[position.x+2][position.y] & PIECE_MASK) === 0 && isKingsHall(state, position.x+2,position.y) || isCorner(state, position.x+2,position.y) ) || (state[position.x+2][position.y] & color) > 0) ){
 			console.log("x+");
 			move.captures.push({x : position.x+1, y : position.y, player : state[position.x+1][position.y]});
 			state[position.x+1][position.y] = 0;
 		}
-		if(position.x-2 >= 0 && (state[position.x-1][position.y] & color) === 0 && ( ( (state[position.x-2][position.y] & PIECE_MASK) === 0 && isKingsHall(position.x-2,position.y) || isCorner(position.x-2,position.y) ) || (state[position.x-2][position.y] & color) > 0) ){
+		if(position.x-2 >= 0 && (state[position.x-1][position.y] & color) === 0 && ( ( (state[position.x-2][position.y] & PIECE_MASK) === 0 && isKingsHall(state, position.x-2,position.y) || isCorner(state, position.x-2,position.y) ) || (state[position.x-2][position.y] & color) > 0) ){
 			console.log("x-");
 			move.captures.push({x : position.x-1, y : position.y, player : state[position.x-1][position.y]});
 			state[position.x-1][position.y] = 0;
 		}
-		if(position.y+2 < size && (state[position.x][position.y+1] & color) === 0 && ( ( (state[position.x][position.y+2] & PIECE_MASK) === 0 && isKingsHall(position.x,position.y+2) || isCorner(position.x,position.y+2) ) || (state[position.x][position.y+2] & color) > 0) ){
+		if(position.y+2 < size && (state[position.x][position.y+1] & color) === 0 && ( ( (state[position.x][position.y+2] & PIECE_MASK) === 0 && isKingsHall(state, position.x,position.y+2) || isCorner(state, position.x,position.y+2) ) || (state[position.x][position.y+2] & color) > 0) ){
 			console.log("y+");
 			move.captures.push({x : position.x, y : position.y+1, player : state[position.x][position.y+1]});
 			state[position.x][position.y+1] = 0;
 		}
-		if(position.y-2 >= 0 && (state[position.x][position.y-1] & color) === 0 && ( ( (state[position.x][position.y-2] & PIECE_MASK) === 0 && isKingsHall(position.x,position.y-2) || isCorner(position.x,position.y-2) ) || (state[position.x][position.y-2] & color) > 0) ){
+		if(position.y-2 >= 0 && (state[position.x][position.y-1] & color) === 0 && ( ( (state[position.x][position.y-2] & PIECE_MASK) === 0 && isKingsHall(state, position.x,position.y-2) || isCorner(state, position.x,position.y-2) ) || (state[position.x][position.y-2] & color) > 0) ){
 			console.log("y-");
 			move.captures.push({x : position.x, y : position.y-1, player : state[position.x][position.y-1]});
 			state[position.x][position.y-1] = 0;
