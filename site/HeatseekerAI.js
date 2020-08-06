097c33f16a1acbc2ecd9ed3cc1669bc6ee95287f
Kyle Harrity <hpkyh47@gmail.com>
2020-08-05 21:45:55 -0400

    ai blocks edge access whenever possible

--- site/HeatseekerAI.js
+++ site/HeatseekerAI.js
@@ -1,5 +1,6 @@
 //currently assumes game is Brandubh
-function HeatseekerAI(type){
+function HeatseekerAI(type, variant){
+	//TODO use variant to load correct variant
 	console.log("heatseeker AI: "+type);
 
 	const E = 0x00;//empty
@@ -22,8 +23,8 @@ function HeatseekerAI(type){
 		//compute dynamic heatmap
 
 		//init map to the static map by cloning it via JSON->stringify->parse
-		let dynamap = JSON.parse(JSON.stringify(BRANDUBH_ATTACKER_HMAP));
-		let pre = JSON.parse(JSON.stringify(BRANDUBH_ATTACKER_HMAP));
+		let dynamap = JSON.parse(JSON.stringify(TABLUT_ATTACKER_HMAP));//BRANDUBH_ATTACKER_HMAP));
+		let pre = JSON.parse(JSON.stringify(TABLUT_ATTACKER_HMAP));//BRANDUBH_ATTACKER_HMAP));
 		console.log(pre);
 		let state = game.getBoard();
 		for(let i=0; i < state.length; i++){
@@ -41,14 +42,14 @@ function HeatseekerAI(type){
 					if(state[i-1] !== undefined && state[i-1][j] === B){
 						dynamap[i+1][j] += 15;	//can capture from above
 						if(dynamap[i-2]){ dynamap[i-2][j] +=  3;}	//can reinforce ally from below
-					}else{
+					}else if(state[i-1] !== undefined){
 						dynamap[i-1][j] += 2;	//can attack
 					}
 
 					if(state[i+1] !== undefined && state[i+1][j] === B){
 						dynamap[i-1][j] += 15;
 						if(dynamap[i+2]){ dynamap[i+2][j] += 3;}
-					}else{
+					}else if(state[i+1] !== undefined){
 						dynamap[i+1][j] += 2;	//can attack
 					}
 
@@ -83,21 +84,28 @@ function HeatseekerAI(type){
 						dynamap[i][j-1] += 1000;
 					}
 					//contain heuristics
+					//TODO prefer moves that block edge, escape access or minimax to find out that not blocking is a next turn win for white
+					let edgeAccess = findEdgeAccess(state, i, j);
+
+					let edgeMod = edgeAccess.left ? 10 : 0;//up score if moving there blocks an open edge/corner
 					for(let x=i-1; x >= 0; x--){
 						let diff = Math.abs(i-x);
-						dynamap[x][j] += 15 - diff;
+						dynamap[x][j] += 15 - diff + edgeMod;
 					}
+					edgeMod = edgeAccess.right ? 10 : 0;
 					for(let x=i+1; x < state.length; x++){
 						let diff = Math.abs(i-x);
-						dynamap[x][j] += 15 - diff;
+						dynamap[x][j] += 15 - diff + edgeMod;
 					}
+					edgeMod = edgeAccess.up ? 10 : 0;
 					for(let y=j-1; y >= 0; y--){
 						let diff = Math.abs(j-y);
-						dynamap[i][y] += 15 - diff;
+						dynamap[i][y] += 15 - diff + edgeMod;
 					}
+					edgeMod = edgeAccess.down ? 10 : 0;
 					for(let y=j+1; y < state[i].length; y++){
 						let diff = Math.abs(j-y);
-						dynamap[i][y] += 15 - diff;
+						dynamap[i][y] += 15 - diff + edgeMod;
 					}
 				}
 				else if(state[i][j] === B){
@@ -132,6 +140,46 @@ function HeatseekerAI(type){
 		//console.log(bestMove);
 		return bestMoves[getRandomInt(0, bestMoves.length)];
 	}
+
+	//returns an object with true false values for each direction:
+	//{ up: <boolean>, down: <boolean>, left: <boolean>, right: <boolean>}
+	function findEdgeAccess(state, kingX, kingY){
+		let retVal = {
+			up : true,   // [0-j)
+			down : true, // (j-length)
+			left : true, // [0-i)
+			right : true // (i-length)
+		};
+		let i = kingX;
+		let j = kingY;
+
+		for(let x=i-1; x >= 0; x--){//left
+			if(state[x][j] !== E){
+				retVal.left = false;
+				break;	
+			}
+		}
+		for(let x=i+1; x < state.length; x++){//right
+			if(state[x][j] !== E){
+				retVal.right = false;
+				break;
+			}
+		}
+		for(let y=j-1; y >= 0; y--){//up
+			if(state[i][y] !== E){
+				retVal.up = false;
+				break;
+			}
+		}
+		for(let y=j+1; y < state[i].length; y++){
+			if(state[i][y] !== E){
+				retVal.down = false;
+				break;
+			}
+		}
+
+		return retVal;
+	}
 }
 
 function getRandomInt(min, max) {
@@ -139,6 +187,8 @@ function getRandomInt(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
+
+
 
 //TODO static positional heatmaps for all variants
 
@@ -160,3 +210,25 @@ BRANDUBH_KING_HMAP =
  [10 , 0, 0, 0, 0, 0, 10],
  [1  ,10, 0, 0, 0,10,  1],
  [100, 1,10,10,10, 1,100]];
+
+TABLUT_ATTACKER_HMAP = 
+[[0,0,1,0,0,0,1,0,0],
+ [0,5,0,0,0,0,0,5,0],
+ [1,0,0,0,0,0,0,0,1],
+ [0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0],
+ [1,0,0,0,0,0,0,0,1],
+ [0,5,0,0,0,0,0,5,0],
+ [0,0,1,0,0,0,1,0,0]];
+
+TABLUT_KING_HMAP = 
+[[100,1,10,10,10,10,10,1,100],
+ [1,10,0,0,0,0,0,10,1],
+ [10,0,0,0,0,0,0,0,10],
+ [10,0,0,0,0,0,0,0,10],
+ [10,0,0,0,0,0,0,0,10],
+ [10,0,0,0,0,0,0,0,10],
+ [10,0,0,0,0,0,0,0,10],
+ [1,10,0,0,0,0,0,10,1],
+ [100,1,10,10,10,10,10,1,100]];
